e7fbabf67ad5b98cf2ee73255cd213d048afe403
Hera <mostafizhera@gmail.com>
2024-09-19 14:45:34 +0600

    added the main content title

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-
+import React from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+        <div className='main'>
         <h2>WELCOME TO
-        Mostafiz's INNOVATION</h2>
+        <span> MOSTAFIZ'S </span> INNOVATION</h2>
+        </div>
     </>
   )
 }
 
-export default App
+export default App;
